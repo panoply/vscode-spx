68102dfc90c551ab8086ca151be4d82dd511ec73
Nikolas Savvidis <nicos@gmx.com>
2024-08-22 00:10:21 +0200

    bundle parser

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: [ './extension/index.ts' ],
   format: 'cjs',
+  noExternal: [ '@typescript-eslint/typescript-estree' ],
   external: [ 'vscode' ]
 });
